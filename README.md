49db8d9341fa1fa5067e26c3d25509d560dd1fb7
soulivanh vorlasane <soulivanh@soulivanhs-MacBook-Pro.local>
2025-09-23 19:16:26 +0700

    init git

---
+++ README.md
@@ -0,0 +1,4 @@
+# bloc_hive_caching_data
+
+A new Flutter project with BLOC HIVE CACHING DATA.
+https://www.youtube.com/watch?v=GHWPfxpkBoM&t=16s
---
+++ android/app/src/main/kotlin/com/example/bloc_hive_caching_data/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.bloc_hive_caching_data
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
